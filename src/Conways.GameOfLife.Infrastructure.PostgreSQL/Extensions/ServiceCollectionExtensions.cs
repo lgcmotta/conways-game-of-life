8fdf8ee1cdb566ef17ebff5970dfcb8767a50a34
lgcmotta <lgcmotta01@gmail.com>
2024-07-01 15:10:41 -0300

    feat: add readonly db context

--- src/Conways.GameOfLife.Infrastructure.PostgreSQL/Extensions/ServiceCollectionExtensions.cs
+++ src/Conways.GameOfLife.Infrastructure.PostgreSQL/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Conways.GameOfLife.Infrastructure.PostgreSQL.Interceptors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -5,26 +6,45 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Conways.GameOfLife.Infrastructure.PostgreSQL.Extensions;
 
+[ExcludeFromCodeCoverage]
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddBoardDbContext(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddBoardDbContexts(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString(nameof(BoardDbContext));
-
+        
+        ArgumentException.ThrowIfNullOrWhiteSpace(connectionString);
+        
         var retries = configuration.GetValue<int>("PostgresSettings:RetryCount");
 
-        services.AddDbContext<BoardDbContext>((provider, builder) =>
+        services.AddBoardDbContext<BoardDbContext>(connectionString, retries);
+        
+        services.AddBoardDbContext<BoardDbContextReadOnly>(connectionString, retries, useInterceptors: false);
+        
+        return services;
+    }
+
+    private static IServiceCollection AddBoardDbContext<TDbContext>(
+        this IServiceCollection services,
+        string connectionString,
+        int retries,
+        bool useInterceptors = true)
+        where TDbContext : DbContext
+    {
+        services.AddDbContext<TDbContext>((provider, builder) =>
         {
             builder.UseNpgsql(connectionString, pgsql =>
             {
                 pgsql.EnableRetryOnFailure(retries);
             });
 
+            if (!useInterceptors) return;
+            
             var interceptors = InterceptorsAssemblyScanner.Scan(provider, typeof(BoardDbContext).Assembly);
 
             builder.AddInterceptors(interceptors);
         });
-        
+
         return services;
     }
 }
